0b24cb375232a1a3d1bf3a1083c3631342dca3a4
IvanPresmytky <ivan.presmytsky@gmail.com>
2022-02-19 20:26:44 +0300

    init: set up client app

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="header">
+        <h1>Knowledge shop</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
