b7c0006c7b00b015b65fc15d15bba1b296660c0a
IvanPresmytky <ivan.presmytsky@gmail.com>
2022-02-20 22:58:12 +0300

    feat: add calculating rating logit to the api

--- api/src/routes/apiRouter.ts
+++ api/src/routes/apiRouter.ts
@@ -1,7 +1,9 @@
-import StatusCodes from 'http-status-codes';
+import StatusCodes, { BAD_REQUEST } from 'http-status-codes';
 import { Router, Request, Response } from 'express';
 
 import productsMock from '@mocks/products.json';
+import { getOverallRating } from 'src/utils/getOverallRating/getOverallRating';
+import { TProduct, TReview } from 'src/types';
 
 const { CREATED, OK, NOT_FOUND } = StatusCodes;
 
@@ -19,6 +21,9 @@ const db = [
 
 router.get(ERoutes.Products, (_req: Request, res: Response) => {
   const products = db;
+  if (!products || products.length === 0) {
+    return res.sendStatus(NOT_FOUND);
+  }
   return res.status(OK).json(products);
 });
 
@@ -35,8 +40,27 @@ router.get(ERoutes.Reviews, (req: Request, res: Response) => {
 
 router.post(ERoutes.Reviews, (req: Request, res: Response) => {
   const { params, body } = req;
-  const product = db.find((item) => item._id === params.id);
-  product?.reviews.push(body);
+
+  const review = body as TReview;
+
+  if(!params.id || !Object.values(review)) {
+    return res.status(BAD_REQUEST);
+  }
+
+  const product = db.find((item) => item._id === params.id) as TProduct;
+
+  if (!product) {
+    return res.status(NOT_FOUND);
+  }
+
+  if (!review.reviewText) {
+    review.reviewText = '';
+  }
+
+  product.reviews.push(review);
+  const newOverallRating = getOverallRating(product.reviews) || product.overallRating;
+  product.overallRating = newOverallRating;
+
   return res.status(CREATED).json(body);
 });
 
